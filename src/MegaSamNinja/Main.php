6520322c8846e9b2ca72164fb622c13b451899ef
Samuel <Samuel_ipad2@hotmail.com>
2014-10-22 16:34:25 +1100

    Create Main.php

---
+++ src/MegaSamNinja/Main.php
@@ -0,0 +1,35 @@
+//
+<?php
+/*
+__PocketMine Plugin__
+name=fall
+description=Teleports you to coords 100,1000,100
+version=1.0
+author=MegaSamNinja
+class=fall
+apiversion=12
+*/
+class Fall implements Plugin{
+   private $api;
+   
+   public function __construct(ServerAPI $api, $server = false){
+     $this->api = $api;
+   }
+   
+    public function init(){
+    $this->api->console->register("fall", "Sends the person in space", array($this, "fall"));
+    $this->api->ban->cmdwhitelist("fall");
+    $this->path = $this->api->plugin->configPath($this);
+    $this->config = new Config($this->path . "Fall.yml", CONFIG_YAML, array("Jail" => "This will be used in a later version"));
+    }
+     
+    public function fall($cmd, $args, $issuer){
+    $username = $issuer->username;
+    $this->api->console->run("tp " . $username . " 100 1000 100", "console");
+    $this->api->chat->sendTo(false, "You have been TELEPORTED by MAGIC", $username);
+   }
+   
+   public function __destruct(){
+   }
+}
+//
