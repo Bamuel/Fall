2bcf1fb6b171a5e32e0bc841a44240ae9649a9d7
Samuel <Samuel_ipad2@hotmail.com>
2015-01-31 11:25:50 +1100

    Update Main.php

--- src/MegaSamNinja/fall/Main.php
+++ src/MegaSamNinja/fall/Main.php
@@ -1,47 +1,35 @@
 <?php
-	namespace megasamninjafall;
-    use pocketminecommandCommand;
-    use pocketminecommandCommandExecutor;
-    use pocketminecommandCommandSender;
-    use pocketminemathVector3;
-    use pocketminePlayer;
-    use pocketminepluginPluginBase;
-
-// -- Class Name : Main
-// -- Purpose : 
-// -- Created On : 
-    class Main extends PluginBase implements CommandExecutor{
-        public
-
-// -- Function Name : onEnable
-// -- Params : 
-// -- Purpose : 
-        function onEnable() {
-            @mkdir($this->getDataFolder());
-            $this->configFile = (new Config($this->getDataFolder()."config.yml", Config::YAML, array(            "Y-Axis" => "1000"        )))->getAll();
-            $this->getLogger()->info("Fall has been enabled.");
+namespace megasamninja\fall;
+use pocketmine\command\Command;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+use pocketmine\math\Vector3;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+class Main extends PluginBase implements CommandExecutor{
+    
+    public function onEnable() {
+    	@mkdir($this->getDataFolder());
+        $this->configFile = (new Config($this->getDataFolder()."config.yml", Config::YAML, array(
+            "Y-Axis" => "1000"
+        )))->getAll();
+        $this->getLogger()->info("Fall has been enabled.");
+    }
+    
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        if($sender instanceof Player){
+           $sender->teleport(new Vector3(100, $this->configFile['Y-Axis'], 100));
+           $sender->sendMessage("You have been TELEPORTED by MAGIC");
+           return true;
         }
-
-        public
-
-// -- Function Name : onCommand
-// -- Params : CommandSender $sender, Command $command, $label, array $args
-// -- Purpose : 
-        function onCommand(CommandSender $sender, Command $command, $label, array $args){
-            
-            if($sender instanceof Player){
-                $sender->teleport(new Vector3(100, $this->configFile['Y-Axis'], 100));
-                $sender->sendMessage("You have been TELEPORTED by MAGIC");
-                return true;
-            } else {
-                $sender->sendMessage("Please run command in game.");
-                $sender->sendMessage("Seriously do you think this plugin will work via Console");
-                return true;
-            }
-
+        else {
+           $sender->sendMessage("Please run command in game.");
+           $sender->sendMessage("Seriously do you think this plugin will work via Console");
+           return true;
         }
-
-    }    
+   }
+}
+    
 /*
 class Fall implements Plugin{
    private $api;
@@ -67,4 +55,3 @@ class Fall implements Plugin{
    }
 }
 */
-
